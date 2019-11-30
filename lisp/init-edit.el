cdf20f66956b5f144c7298e4aa601565416280fc
Weizhe Li <weizheli@Weizhes-iMac-2.fios-router.home>
2019-11-30 13:31:19 -0500

    more add ons.

---
+++ lisp/init-edit.el
@@ -0,0 +1,13 @@
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(browse-kill-ring-default-keybindings)
+
+(move-text-default-bindings)
+
+
+
+(provide 'init-edit)
