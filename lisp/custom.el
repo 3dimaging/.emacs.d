971a6c7484b0a4cb8a740476c3b1244dcab5c0cb
3dimaging <weizheli@gmail.com>
2020-03-06 08:23:07 -0500

    a little modification for themes.

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,15 +8,45 @@
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
    (quote
-    ("a83f05e5e2f2538376ea2bfdf9e3cd8b7f7593b16299238c1134c1529503fa88" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a455366c5cdacebd8adaa99d50e37430b0170326e7640a688e9d9ad406e2edfd" "e1ecb0536abec692b5a5e845067d75273fe36f24d01210bf0aa5842f2a7e029f" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "05fd42bef42991c45ecb17ecad1ff38c10e8faac51efea44f0782bdaebaf4211" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+    ("bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" "85d1dbf2fc0e5d30f236712b831fb24faf6052f3114964fdeadede8e1b329832" "a83f05e5e2f2538376ea2bfdf9e3cd8b7f7593b16299238c1134c1529503fa88" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a455366c5cdacebd8adaa99d50e37430b0170326e7640a688e9d9ad406e2edfd" "e1ecb0536abec692b5a5e845067d75273fe36f24d01210bf0aa5842f2a7e029f" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "05fd42bef42991c45ecb17ecad1ff38c10e8faac51efea44f0782bdaebaf4211" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
  '(evil-leader/leader "SPC")
  '(evil-want-C-u-scroll t)
+ '(fci-rule-color "#505050")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1b1d1e" "#fc20bb"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1b1d1e" "#60aa00"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1b1d1e" "#505050"))
+ '(objed-cursor-color "#d02b61")
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
     (doom-themes company-box all-the-icons-ibuffer all-the-icons-ivy-rich spaceline spaceline-all-the-icons ztree grip-mode pyimport elpygen indent-tools hydra flx-isearch dired-du circe python-docstring sphinx-doc company-anaconda avy-flycheck avy-menu ace-jump-mode helm-projectile evil-iedit-state evil-multiedit fill-column-indicator auto-highlight-symbol highlight-symbol crux browse-kill-ring doom-modeline dashboard beacon helm magit-todos 4clojure 0blayout powerline window-number ivy-mpdel color-theme-modern calfw calfw-cal calfw-gcal calfw-howm calfw-ical calfw-org spacemacs-theme moe-theme org-beautify-theme org move-text all-the-icons all-the-icons-dired all-the-icons-gnus all-the-icons-ivy neotree leuven-theme nimbus-theme ob-ipython dired-icon dired-imenu discover-my-major nyan-mode company-jedi projectile projectile-codesearch projectile-git-autofetch counsel-projectile avy lispy rainbow-delimiters dired-details+ dired-rainbow dired-sidebar dired-sort dired-sort-menu dired-sort-menu+ diredful latex-extra latex-math-preview latex-pretty-symbols latex-unicode-math-mode ess ess-R-data-view ess-smart-equals ess-smart-underscore ess-view markdown-mode markdown-mode+ color-theme ahungry-theme dired+ tabbar-ruler treemacs treemacs-evil treemacs-projectile auctex use-package 2048-game auto-yasnippet cdlatex counsel ein elpy evil-leader evil-nerd-commenter evil-surround exec-path-from-shell expand-region find-file-in-project flycheck ggtags helm-ag helm-gtags highlight-indentation hungry-delete iedit js2-refactor latex-preview-pane lua-mode magit magit-popup monokai-theme multiple-cursors mwe-log-commands nodejs-repl org-pomodoro pallet pkg-info popup popwin powerline-evil py-autopep8 pyvenv request-deferred reveal-in-osx-finder s shut-up skewer-mode smartparens smartrep solarized-theme swiper undo-tree web-mode websocket which-key window-numbering with-editor yasnippet auctex-latexmk auctex-lua auto-complete-auctex company-auctex company-ebdb company-math company-statistics company)))
+ '(pdf-view-midnight-colors (cons "#dddddd" "#1b1d1e"))
+ '(rustic-ansi-faces
+   ["#1b1d1e" "#d02b61" "#60aa00" "#d08928" "#6c9ef8" "#b77fdb" "#00aa80" "#dddddd"])
  '(tramp-copy-size-limit 1000000)
- '(tramp-inline-compress-start-size 1000000))
+ '(tramp-inline-compress-start-size 1000000)
+ '(vc-annotate-background "#1b1d1e")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#60aa00")
+    (cons 40 "#859f0d")
+    (cons 60 "#aa931a")
+    (cons 80 "#d08928")
+    (cons 100 "#d38732")
+    (cons 120 "#d6863d")
+    (cons 140 "#da8548")
+    (cons 160 "#ce8379")
+    (cons 180 "#c281aa")
+    (cons 200 "#b77fdb")
+    (cons 220 "#bf63b2")
+    (cons 240 "#c74789")
+    (cons 260 "#d02b61")
+    (cons 280 "#b0345c")
+    (cons 300 "#903d58")
+    (cons 320 "#704654")
+    (cons 340 "#505050")
+    (cons 360 "#505050")))
+ '(vc-annotate-very-old-color nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
