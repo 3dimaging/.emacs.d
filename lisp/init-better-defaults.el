364fc1802df842c59517d4d5b9fe28270a0d9c59
Weizhe Li <weizheli@Weizhes-iMac-2.fios-router.home>
2020-04-25 09:30:22 -0400

    add lispy, white space etc. to better-defaults

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,4 +1,9 @@
-(setq ring-bell-function 'ignore) 
+;;; better-default ---Summary
+
+;;; better-default ---Commentary
+;;; code:
+
+(setq ring-bell-function 'ignore)
 
 (global-linum-mode t)
 (global-auto-revert-mode t)
@@ -88,4 +93,60 @@
 ;; set the seleted words or region as blue with background as white.
 (set-face-attribute 'region nil :background "#666" :foreground "#0000FF")
 
+(use-package saveplace
+  :ensure nil
+  :hook (after-init . save-place-mode))
+
+;; (use-package so-long
+  ;; :ensure nil
+  ;; :config (global-so-long-mode 1))
+
+;; (show-trailing-whitespace t)
+(use-package whitespace
+  :ensure nil
+  :hook (after-init . global-whitespace-mode) ;; 注意，这里是全局打开
+  :config
+  ;; Don't use different background for tabs.
+  (face-spec-set 'whitespace-tab
+                 '((t :background unspecified)))
+  ;; Only use background and underline for long lines, so we can still have
+  ;; syntax highlight.
+
+  ;; For some reason use face-defface-spec as spec-type doesn't work.  My guess
+  ;; whitespace.el.  Anyway, we have to manually set some attribute to
+  ;; unspecified here.
+  (face-spec-set 'whitespace-line
+                 '((((background light))
+                    :background "#d8d8d8" :foreground unspecified
+                    :underline t :weight unspecified)
+                   (t
+                    :background "#404040" :foreground unspecified
+                    :underline t :weight unspecified)))
+
+  ;; Use softer visual cue for space before tabs.
+  (face-spec-set 'whitespace-space-before-tab
+                 '((((background light))
+                    :background "#d8d8d8" :foreground "#de4da1")
+                   (t
+                    :inherit warning
+                    :background "#404040" :foreground "#ee6aa7")))
+
+  (setq
+   whitespace-line-column nil
+   whitespace-style
+   '(face             ; visualize things below:
+     empty            ; empty lines at beginning/end of buffer
+     lines-tail       ; lines go beyond `fill-column'
+     space-before-tab ; spaces before tab
+     trailing         ; trailing blanks
+     tabs             ; tabs (show by face)
+     tab-mark         ; tabs (show by symbol)
+     )))
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+(defun conditionally-enable-lispy ()
+  (when (eq this-command 'eval-expression)
+    (lispy-mode 1)))
+(add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
 (provide 'init-better-defaults)
+
+;;; init-better-default.el ends here.
